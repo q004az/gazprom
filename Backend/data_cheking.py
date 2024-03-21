8ec53e4dcd13d75c64522c476fad66ad23b7bfb5
lera <alisagrotesk@gmail.com>
2024-03-21 13:23:18 +0700

    data_cheking

--- Backend/data_cheking.py
+++ Backend/data_cheking.py
@@ -1,38 +1,37 @@
-
-def is_correct_data_user(surname: str, name: str, patronymic: str, login: str, password: str)-> bool, {'surname' : bool, 'name' : bool, patronymic : bool, login : bool, password : bool}
+def is_correct_data_user(surname: str, name: str, patronymic: str, login: str, password: str)-> (bool, dict):
     is_correct_data = {'surname': True,'name': True, 'patronymic': True,'login': True,'password': True}
     digits = '1234567890'
     upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
     lower_letters = 'abcdefghijklmnopqrstuvwxyz'
     symbols = '!@#$%^&*()-+'
     acceptable = digits+upper_letters+lower_letters+symbols
-        if any(char not in acceptable for char in password):
+    if any(char not in acceptable for char in password):
+        return False
+    elif len(password) > 8:
+        return True
+    else:
+        if len(password) < 8:
             return False
-        elif len(password) > 8:
+    if any(char not in acceptable for char in login):
+        return False
+    elif len(login) < 8:
+        return False
+    else:
+        if len(login) > 8:
             return True
-        else:
-            if len(password) < 8:
-                return False
-        if any(char not in acceptable for char in login):
-            return False
-        elif len(login) < 8:
-            return False
-        else:
-            if len(login) > 8:
-                return True
-        if surname == acceptable and surname == []:
-            return False
-        else:
-            if len(surname) > 30:
-                return False
-if name == acceptable and name == []:
-    return False
-else:
-    if len(name) > 30:
+    if surname == acceptable and surname == []:
         return False
-if patronymic == acceptable and patronymic == []:
-    return False
-else:
-    if len(patronymic) > 30:
+    else:
+        if len(surname) > 30:
+            return False
+    if name == acceptable and name == []:
         return False
+    else:
+        if len(name) > 30:
+            return False
+    if patronymic == acceptable and patronymic == []:
+        return False
+    else:
+        if len(patronymic) > 30:
+            return False
 
